96b1a6ff6dd0885e5fd97e3de1f2984b28076b1c
vergeslich03 <vergeslichlp@gmail.com>
2026-08-19 23:30:02 +0200

    removed logging/dumping logic

--- client/PunchcardManager.cs
+++ client/PunchcardManager.cs
@@ -1,7 +1,5 @@
 using Il2CppSleepyNodes;
-using Il2CppInterop.Runtime;
 using HarmonyLib;
-using MelonLoader;
 
 namespace APNestClient;
 
@@ -10,21 +8,6 @@ public class PunchcardManager
 {
     static void Postfix(StateNode __instance)
     {
-        var classPtr = IL2CPP.il2cpp_object_get_class(__instance.Pointer);
-        var nodeType = IL2CPP.il2cpp_class_get_name_(classPtr);
-
-        string nodeId = __instance.NodeID;
-
-        string graphType = "null";
-        string graphName = "null";
-        var graph = __instance.graph;
-        if (graph != null)
-        {
-            var graphClassPtr = IL2CPP.il2cpp_object_get_class(graph.Pointer);
-            graphType = IL2CPP.il2cpp_class_get_name_(graphClassPtr);
-            graphName = graph.name;
-        }
-
-        MelonLogger.Msg("Node: " + nodeType + " id=" + nodeId + " graph=" + graphType + " (" + graphName + ")");
+        // TODO unlocking of Punchcards
     }
 }
