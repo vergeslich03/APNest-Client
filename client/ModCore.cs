7dd1c641067233bb38e52293c3cf7ce89ae0c13d
vergeslich03 <vergeslichlp@gmail.com>
2026-09-02 23:58:09 +0200

    fixed error, where duplicate init of ModConfig prevented Mod from loading

--- client/ModCore.cs
+++ client/ModCore.cs
@@ -13,7 +13,7 @@ namespace APNestClient
         {
             Logger.Msg("APNest Client loaded.");
 
-            _connectUI = new ConnectUI(new ModConfig());
+            _connectUI = new ConnectUI(config);
             _apSession = _connectUI.GetApSession();
             _itemReceiver = new ItemReceiver();
             _mainMenuAPHook = new MainMenuAPHook(_connectUI);
